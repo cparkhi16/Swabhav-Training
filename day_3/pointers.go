8bb37646d0957fe6f5ab77a86e5963f92d8e98db
cparkhi16@hmail.com <cparkhi16@gmail.com>
2021-11-12 10:02:26 +0530

    Pointers and Function returning a function

---
+++ day_3/pointers.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func addition(n1, n2 float32) float32 {
+	ans := n1 + n2
+	return ans
+}
+func main() {
+	f := a()
+	ans := f(float32(10), float32(30), addition)
+	fmt.Println(ans)
+	var a string = "abc"
+	ptr := &a
+	fmt.Println("Value of ptr ", ptr)
+	fmt.Println("Value of a", a)
+	fmt.Println("Value of *ptr", *ptr)
+	ptr2 := &ptr
+	fmt.Println("Value of ptr2 ", ptr2)
+	fmt.Println("Value of *ptr", *ptr2)
+	fmt.Println("Value of **ptr2 ", **ptr2)
+}
+func a() func(f1, f2 float32, f func(num1, num2 float32) float32) float64 {
+	b := func(f1, f2 float32, f func(num1, num2 float32) float32) float64 {
+		return float64(f(f1, f2))
+	}
+	return b
+}
