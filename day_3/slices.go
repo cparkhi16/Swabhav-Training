151667b105192e594786852ca364d4ef31101506
cparkhi16@hmail.com <cparkhi16@gmail.com>
2021-11-15 09:57:29 +0530

    Tried re-slicing

--- day_3/slices.go
+++ day_3/slices.go
@@ -8,6 +8,9 @@ func main() {
 	slice[0] = 1
 	slice[1] = 2
 	slice[3] = 10
+	aar := []int{9, 0}
+	aar = append(aar, 57, 78)
+	fmt.Println(aar)
 	fmt.Println("Slice before func call", slice)
 	fmt.Println("Adress of slice[0] in main ", &slice[0])
 	manipulateSlice(slice)
@@ -17,9 +20,11 @@ func main() {
 	newSlice[1] = 800
 	fmt.Println("New slice before append", newSlice)
 	fmt.Println("Adress of newSlice[0] before append", &newSlice[0])
+	fmt.Printf("Address using printf %p", &newSlice[0])
 	newSlice = append(newSlice, 1000)
-	fmt.Println("New slice after append", newSlice)
+	fmt.Println("\nNew slice after append", newSlice)
 	fmt.Println("Adress of newSlice[0] after append", &newSlice[0])
+	fmt.Printf("Address using printf %p after appending ------\n", &newSlice[0])
 	modifiedSlice := make([]int, 5, 5)
 	modifiedSlice[1] = 8
 	modifiedSlice[2] = 777
@@ -45,11 +50,30 @@ func main() {
 		}
 	}
 	fmt.Println("Slicing a pointers slice ", ptrOfSlices[0:2])
+
+	var abc []int
+	abc = append(abc, 1)
+	fmt.Println(abc)
 }
 func manipulateSlice(s []int) {
 	fmt.Println("Adress of slice[0] in func call ", &s[0])
 	s[0] = 100
 	s[1] = 200
 	s[3] = 300
+	s = append(s, 7000) //MAIN DIFFERENCE TO NOTICE APPEND CHANGES NOT REFLECTS IN MAIN SINCE ADDRESS OF SLICE IS CHANGING
+	fmt.Println("Address after append in func call", &s[0])
 	fmt.Println("Slice in func call", s)
+	fmt.Println()
+	newSlice := []int{1, 2, 3, 4, 5}
+	reSlice := newSlice[1:4]
+	fmt.Println("Re slicing a slice ", reSlice, len(reSlice), cap(reSlice))
+	nSlice := reSlice[2:]
+	fmt.Println(" a reSlice ", reSlice, len(reSlice), cap(reSlice))
+	fmt.Println("nSlice ", nSlice, len(nSlice), cap(nSlice))
+	fmt.Println()
+	a := []int{1, 2, 3, 4, 5, 6}
+	b := a[1:3:5]
+	c := a[1:3]
+	fmt.Println("b ", b, len(b), cap(b))
+	fmt.Println("c ", c, len(c), cap(c))
 }
