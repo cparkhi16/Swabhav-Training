ba912a0474b24c260e75ba25ea2353037e198138
cparkhi16@hmail.com <cparkhi16@gmail.com>
2021-11-16 20:39:11 +0530

    Chapter6.go

---
+++ Exercises/chapter6.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := make([]float64, 3)
+	numbers[0] = 19.7
+	numbers[2] = 25.2
+	for i, number := range numbers {
+		fmt.Println(i, number)
+	}
+	var letters = []string{"a", "b", "c"}
+	for i, letter := range letters {
+		fmt.Println(i, letter)
+	}
+	array := [5]string{"a", "b", "c", "d", "e"}
+	slice := array[1:3]
+	slice = append(slice, "x")
+	slice = append(slice, "y", "z")
+	for _, letter := range slice {
+		fmt.Println(letter)
+	}
+
+}
