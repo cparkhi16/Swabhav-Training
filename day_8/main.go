21071adfc34170ccd34a77e156f31b2031735ba3
cparkhi16@hmail.com <cparkhi16@gmail.com>
2021-11-29 21:48:41 +0530

    Changed name of slice to users

--- day_8/main.go
+++ day_8/main.go
@@ -7,18 +7,18 @@ import (
 )
 
 //var wg sync.WaitGroup
-var user []*hotel.User
+var users []*hotel.User
 
 func main() {
 	userOne := hotel.NewUser("Chinmay J Parkhi", "9823378652")
 	userTwo := hotel.NewUser("Keyur Parkhi", "7689901234")
 	userThree := hotel.NewUser("Rajesh M", "9989901234")
-	user = append(user, userOne, userTwo, userThree)
+	users = append(users, userOne, userTwo, userThree)
 	userOne.NewItemForUser("C", 312, 120, 111)
-	userTwo.NewItemForUser("D", 302, 180, 181)
+	//userTwo.NewItemForUser("D", 302, 180, 181)
 
 	wg := &sync.WaitGroup{}
-	for index, val := range user {
+	for index, val := range users {
 		wg.Add(1)
 		go hotel.PlaceOrder(val, uint(index)+1, uint(index)+100, uint(index)+160, "B", wg)
 	}
