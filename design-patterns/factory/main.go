8e56b7f573f093c4d46c2fce9f397479b9d5fb0b
cparkhi16@hmail.com <cparkhi16@gmail.com>
2021-12-02 09:58:10 +0530

    Updated main.go

--- design-patterns/factory/main.go
+++ design-patterns/factory/main.go
@@ -3,9 +3,13 @@ package main
 func main() {
 	autofactory := &AutomobileFactory{}
 	model := autofactory.Make(bmw)
-	model.Start()
-	model.Stop()
+	if model != nil {
+		model.Start()
+		model.Stop()
+	}
 	modelTwo := autofactory.Make(tesla)
-	modelTwo.Start()
-	modelTwo.Stop()
+	if modelTwo != nil {
+		modelTwo.Start()
+		modelTwo.Stop()
+	}
 }
