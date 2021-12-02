809cc3597e8e45d008b6b74484bdae0fe3a4fd85
cparkhi16@hmail.com <cparkhi16@gmail.com>
2021-12-02 14:21:19 +0530

    Factory comment

--- design-patterns/factory/main.go
+++ design-patterns/factory/main.go
@@ -1,5 +1,6 @@
 package main
 
+// Factory pattern is used when we want to create any object avaialable to be created which implements a interface.
 func main() {
 	autofactory := &AutomobileFactory{}
 	model := autofactory.Make(bmw)
