022c5aef7a776518a843490bc389934e3788d176
cparkhi16@hmail.com <cparkhi16@gmail.com>
2022-01-03 16:53:28 +0530

    Changed query for getting countries with no states

--- go-advance/gorm/exercise/repo/main.go
+++ go-advance/gorm/exercise/repo/main.go
@@ -33,16 +33,16 @@ func main() {
 	employeeService.GetEmployeeDeptNames(&ep, "emp.ename,dept.dname", "join dept on emp.deptno = dept.deptno")
 	employeeService.GetDeptNameWiseCount(&ep, "count(*),dname", "dname", "join dept on emp.deptno=dept.deptno")
 	employeeService.GetDeptNameJobWiseCount(&ep, "count(*),dname,job", "dname,job", "join dept on emp.deptno=dept.deptno")
-	employeeService.GetEmpNameDeptNameWithNoEmployeeAsNull(&dept, "ename as ENAME,dname as DNAME,job as JOB", "left join emp on emp.deptno=dept.deptno")
-	employeeService.GetDeptNameWhereEmployeesAreNull(&dept, "dname as DeptName", "left join emp on emp.deptno=dept.deptno")
+	employeeService.GetEmpNameDeptNameWithNoEmployeeAsNull(&dept, "ename as ENAME,dept.dname as DNAME,job as JOB", "left join emp on emp.deptno=dept.deptno")
+	employeeService.GetDeptNameWhereEmployeesAreNull(&dept, "dept.dname as DeptName", "left join emp on emp.deptno=dept.deptno")
 	employeeService.GetEmployeeAndBossName(&ep, "e.ename,m.ename", "e join emp m on e.mgr=m.empno")
 	//If no boss give null -> //SELECT  e.ename 'Employee', m.ename 'Manager' FROM emp e left join emp m ON (e.mgr = m.empno);
 	employeeService.GetEmployeeAndBossName(&ep, "e.ename,m.ename", "e left join emp m on e.mgr=m.empno")
 	//SELECT  e.ename 'Employee', m.ename 'Manager',dname FROM emp e left join emp m ON (e.mgr = m.empno) join dept d ON e.deptno=d.deptno ;
-	employeeService.GetEmpBossAndDeptName(&ep, "e.ename as ENAME ,m.ename as ENAME ,dname as DNAME", "e left join emp m on e.mgr=m.empno", " join dept d on e.deptno=d.deptno")
+	employeeService.GetEmpBossAndDeptName(&ep, "e.ename as ENAME ,m.ename as ENAME ,d.dname as DNAME", "e left join emp m on e.mgr=m.empno", " join dept d on e.deptno=d.deptno")
 	regionService.GetRegionsWithNoCountries(&reg, "region_name", "left join countries on regions.region_id=countries.REGION_ID")
 	//select country_name from countries left join locations on countries.COUNTRY_ID=locations.COUNTRY_ID group by countries.country_id having count(state_province)=0
-	regionService.GetCountriesWithNoStates(&c, "`countries`.*", "left join locations on countries.COUNTRY_ID = locations.COUNTRY_ID", "countries.country_id")
+	regionService.GetCountriesWithNoStates(&c, "`countries`.*", " join locations on countries.COUNTRY_ID = locations.COUNTRY_ID", "countries.country_id")
 	//select regions.region_name,countries.country_name,locations.STATE_PROVINCE from regions join countries ON regions.region_id = countries.REGION_ID left join locations on countries.country_id = locations.country_id;
 	regionService.GetRegionCountryState(&reg, "regions.region_name,countries.country_name,locations.STATE_PROVINCE", "left join countries ON regions.region_id = countries.REGION_ID", "left join locations on countries.country_id = locations.country_id")
 
