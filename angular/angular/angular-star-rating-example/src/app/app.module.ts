9a1274cf2294f7e6cd80306ac0d365c12139942d
cparkhi16@hmail.com <cparkhi16@gmail.com>
2022-01-07 10:45:24 +0530

    Star ratings and Observable

---
+++ angular/angular/angular-star-rating-example/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { ObservableComponent } from './observable/observable.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    ObservableComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/angular/angular-star-rating-example/src/app/observable/observable.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-observable',
+  templateUrl: './observable.component.html',
+  styleUrls: ['./observable.component.css']
+})
+export class ObservableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // this.getAsync3().subscribe((data)=>{
+    //   console.log("Data from observable ",data)
+    // },(err)=>{
+    //   console.log("Error ",err)
+    // },()=>{
+    //   console.log("Observable complete")
+    // })
+    //this.getAsync2()
+    this.getAsync4()
+    
+  }
+  getAsync1():Observable<any>{
+    const obs= new Observable<any>((observer)=>{
+      observer.next(10)
+      observer.next(80)
+      //throw new Error("a error occured ")
+      observer.complete()
+    })
+    return obs
+  }
+  getAsync2(){
+    const n=interval(2000)
+    n.subscribe((data)=>{
+      console.log("Data returned from interval observable ",data)
+    })
+  }
+  getAsync3():Observable<any>{
+    return interval(2000).pipe(map((n,i)=>{
+      return {"Data":n,"Time":new Date(),"Index":i}
+    }))
+  }
+  getAsync4(){
+    let srcName = from(['John', 'Tom', 'Katy'])
+  srcName
+   .pipe(map(data => {
+     return data.toUpperCase();
+   }))
+   .subscribe(data => console.log(data))
+}
+}
