cd5ad181766e81cc9bf2b4b474b6178722cf2b2b
Chinmay <hellonothing@something.com>
2022-02-02 12:37:21 +0530

    Combined bell la padula and biba model

--- cryptography/ac/model/file.go
+++ cryptography/ac/model/file.go
@@ -1,10 +1,11 @@
 package model
 
 type File struct {
-	FileName string
-	Level    string
+	FileName  string
+	LevelBIBA string
+	LevelBell string
 }
 
-func NewFile(name, level string) *File {
-	return &File{FileName: name, Level: level}
+func NewFile(name, bell, biba string) *File {
+	return &File{FileName: name, LevelBIBA: biba, LevelBell: bell}
 }
--- cryptography/ac/tq.go
+++ cryptography/ac/tq.go
@@ -6,7 +6,6 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
 	"strconv"
@@ -122,41 +121,42 @@ func AddFiles(file model.File) {
 	files = append(files, file)
 }
 
-func CheckWriteAccessForFiles(user *model.User, mode string) {
-	if mode == "BELL" {
-		userBellLevel, _ := strconv.Atoi(user.LevelBELL)
-		for _, val := range files {
-			//fmt.Println(val)
-			fileLevel, _ := strconv.Atoi(val.Level)
-			if userBellLevel > fileLevel {
-				fmt.Println("You do not have write permissions for  ", val.FileName)
-			} else {
-				fmt.Println("Do you want to write to the file ", val.FileName)
-				var response string
-				fmt.Scan(&response)
-				if response == "Y" {
-					WriteToCSVFile(val.FileName)
-				}
-			}
-		}
-	} else if mode == "BIBA" {
-		userBIBALevel, _ := strconv.Atoi(user.LevelBIBA)
-		for _, val := range files {
-			//fmt.Println(val)
-			fileLevel, _ := strconv.Atoi(val.Level)
-			if userBIBALevel < fileLevel {
-				fmt.Println("You do not have write permissions for  ", val.FileName)
-			} else {
-				fmt.Println("Do you want to write to the file ", val.FileName)
-				var response string
-				fmt.Scan(&response)
-				//fmt.Println("My response ", response)
-				if response == "Y" {
-					WriteToCSVFile(val.FileName)
-				}
+func CheckWriteAccessForFiles(user *model.User) {
+	userBIBALevel, _ := strconv.Atoi(user.LevelBIBA)
+	userBellLevel, _ := strconv.Atoi(user.LevelBELL)
+	for _, val := range files {
+		//fmt.Println(val)
+		fileBellLevel, _ := strconv.Atoi(val.LevelBell)
+		fileBIBALevel, _ := strconv.Atoi(val.LevelBIBA)
+		if userBellLevel > fileBellLevel && userBIBALevel < fileBIBALevel {
+			fmt.Println("You do not have write permissions for  ", val.FileName)
+		} else {
+			fmt.Println("Do you want to write to the file ", val.FileName)
+			var response string
+			fmt.Scan(&response)
+			if response == "Y" {
+				WriteToCSVFile(val.FileName)
 			}
 		}
 	}
+	// } else if mode == "BIBA" {
+	// 	userBIBALevel, _ := strconv.Atoi(user.LevelBIBA)
+	// 	for _, val := range files {
+	// 		//fmt.Println(val)
+	// 		fileLevel, _ := strconv.Atoi(val.Level)
+	// 		if userBIBALevel < fileLevel {
+	// 			fmt.Println("You do not have write permissions for  ", val.FileName)
+	// 		} else {
+	// 			fmt.Println("Do you want to write to the file ", val.FileName)
+	// 			var response string
+	// 			fmt.Scan(&response)
+	// 			//fmt.Println("My response ", response)
+	// 			if response == "Y" {
+	// 				WriteToCSVFile(val.FileName)
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
 func ReadCSVFile(fileName string) {
 	// f, err := os.Open(fileName)
@@ -205,32 +205,33 @@ func ReadCSVFile(fileName string) {
 		fmt.Println(tmp)
 	}
 }
-func CheckReadAccessForFiles(user *model.User, mode string) {
-	if mode == "BELL" {
-		userBellLevel, _ := strconv.Atoi(user.LevelBELL)
-		for _, val := range files {
-			//fmt.Println(val)
-			fileLevel, _ := strconv.Atoi(val.Level)
-			if userBellLevel < fileLevel {
-				fmt.Println("You do not have read permissions for  ", val.FileName)
-			} else {
-				fmt.Println("Contents from ", val.FileName, "----")
-				ReadCSVFile(val.FileName)
-			}
-		}
-	} else if mode == "BIBA" {
-		userBIBALevel, _ := strconv.Atoi(user.LevelBIBA)
-		for _, val := range files {
-			//fmt.Println(val)
-			fileLevel, _ := strconv.Atoi(val.Level)
-			if userBIBALevel > fileLevel {
-				fmt.Println("You do not have read permissions for  ", val.FileName)
-			} else {
-				fmt.Println("Contents from ", val.FileName, "----")
-				ReadCSVFile(val.FileName)
-			}
+func CheckReadAccessForFiles(user *model.User) {
+	userBellLevel, _ := strconv.Atoi(user.LevelBELL)
+	userBIBALevel, _ := strconv.Atoi(user.LevelBIBA)
+	for _, val := range files {
+		//fmt.Println(val)
+		fileBellLevel, _ := strconv.Atoi(val.LevelBell)
+		fileBIBALevel, _ := strconv.Atoi(val.LevelBIBA)
+		if userBellLevel < fileBellLevel && userBIBALevel > fileBIBALevel {
+			fmt.Println("You do not have read permissions for  ", val.FileName)
+		} else {
+			fmt.Println("Contents from ", val.FileName, "----")
+			ReadCSVFile(val.FileName)
 		}
 	}
+	// } else if mode == "BIBA" {
+
+	// 	for _, val := range files {
+	// 		//fmt.Println(val)
+	// 		fileLevel, _ := strconv.Atoi(val.Level)
+	// 		if userBIBALevel > fileLevel {
+	// 			fmt.Println("You do not have read permissions for  ", val.FileName)
+	// 		} else {
+	// 			fmt.Println("Contents from ", val.FileName, "----")
+	// 			ReadCSVFile(val.FileName)
+	// 		}
+	// 	}
+	// }
 }
 func WriteFile(filename string, fileMode os.FileMode, data string) error {
 	file, err := os.OpenFile(filename, int(fileMode), 0777)
@@ -254,51 +255,49 @@ func ReadFile(filename string) (string, error) {
 	return string(aes.Decrypt(data, "hello")), nil
 }
 func main() {
-	// file := model.NewFile("employee.csv", "2") // no write // BELL
-	// AddFiles(*file)
-	// fileTwo := model.NewFile("items.csv", "4") // no read //BELL
-	// AddFiles(*fileTwo)
-	// fmt.Println("1.Register 2.Login ")
-	// var response string
-	// fmt.Scanln(&response)
-	// if response == "1" {
-	// 	fmt.Println("Enter name password biba and bell levels")
-	// 	var name string
-	// 	var password string
-	// 	var biba string
-	// 	var bell string
-	// 	fmt.Scanf("%s %s %s %s", &name, &password, &biba, &bell)
-	// 	user := model.NewUser(name, password, biba, bell)
-	// 	user.AddUser()
-	// 	mode := "BIBA"
-	// 	CheckWriteAccessForFiles(user, mode)
-	// 	CheckReadAccessForFiles(user, mode)
-	// } else {
-	// 	//user := model.NewUser("Chinmay", "test", "3", "3")
-	// 	user := model.Login()
-	// 	if user == nil {
-	// 		fmt.Println("Invalid username or password ")
-	// 	} else {
-	// 		mode := "BELL"
-	// 		CheckWriteAccessForFiles(user, mode)
-	// 		CheckReadAccessForFiles(user, mode)
-	// 	}
-	// }
+	file := model.NewFile("employee.csv", "2", "3") // no write // BELL
+	AddFiles(*file)
+	fileTwo := model.NewFile("items.csv", "3", "2") // no read //BELL
+	AddFiles(*fileTwo)
+	fmt.Println("1.Register 2.Login ")
+	var response string
+	fmt.Scanln(&response)
+	if response == "1" {
+		fmt.Println("Enter name password biba and bell levels")
+		var name string
+		var password string
+		var biba string
+		var bell string
+		fmt.Scanf("%s %s %s %s", &name, &password, &biba, &bell)
+		user := model.NewUser(name, password, biba, bell)
+		user.AddUser()
+		CheckWriteAccessForFiles(user)
+		CheckReadAccessForFiles(user)
+	} else {
+		//user := model.NewUser("Chinmay", "test", "3", "3")
+		user := model.Login()
+		if user == nil {
+			fmt.Println("Invalid username or password ")
+		} else {
+			CheckWriteAccessForFiles(user)
+			CheckReadAccessForFiles(user)
+		}
+	}
 	// employeeCSV()
 	//itemsCSV()
 	// user := model.NewUser("Chinmay", "test", "3", "3")
 	// CheckWriteAccessForFiles(user)
 	// CheckReadAccessForFiles(user)
-	var data string
-	fmt.Println("Enter data")
-	fmt.Scan(&data)
-	err := WriteFile("test.txt", fs.FileMode(os.O_APPEND), data)
-	if err != nil {
-		fmt.Println("-= Error while writing to file -=", err)
-	}
-	d, err := ReadFile("test.txt")
-	if err != nil {
-		fmt.Println("-= Error reading file -=", err)
-	}
-	fmt.Println("Contents of  file ", d)
+	// var data string
+	// fmt.Println("Enter data")
+	// fmt.Scan(&data)
+	// err := WriteFile("test.txt", fs.FileMode(os.O_APPEND), data)
+	// if err != nil {
+	// 	fmt.Println("-= Error while writing to file -=", err)
+	// }
+	// d, err := ReadFile("test.txt")
+	// if err != nil {
+	// 	fmt.Println("-= Error reading file -=", err)
+	// }
+	// fmt.Println("Contents of  file ", d)
 }
