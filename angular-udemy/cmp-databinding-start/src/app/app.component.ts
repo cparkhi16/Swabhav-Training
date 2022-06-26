d804f233485837d36b5340224754a61d4dcbd47d
chinmay-parkhi <chinmay.parkhi@forcepoint.com>
2022-06-26 18:00:37 +0530

    Lifecycle hooks , using viewchild and contentchild with ngAfterViewInit and ngAfterContentInit , ngDoChange only gets fired for primitive data type ('name') and not for objects (as they are reference types)

--- angular-udemy/cmp-databinding-start/src/app/app.component.ts
+++ angular-udemy/cmp-databinding-start/src/app/app.component.ts
@@ -21,5 +21,12 @@ export class AppComponent {
       content: blueprintData.serverContent
     });
   }
+  onChangeFirst(){
+    console.log(" Onchange first called ")
+    this.serverElements[0].name="Changed !"
+  }
+  onDestroyFirst(){
+    this.serverElements.splice(0,1)
+  }
   
 }
--- angular-udemy/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ angular-udemy/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,11 +7,41 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   //encapsulation : ViewEncapsulation.Emulated // by default it is emulated that is view encapsulation is enabled ( css to component binding)
   //encapsulation : ViewEncapsulation.None // it disables view encapsulation
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit ,OnChanges,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy{
   @Input('srvElement') element : {type:string,name:string,content:string};
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() name:string;
+  @ViewChild('heading',{static:true}) heading:ElementRef;
+  @ContentChild('paragraph',{static:true}) paragraph:ElementRef;
+  constructor() { 
+    console.log("-- constructor called --")
   }
 
+  ngOnInit(): void {
+    console.log("ng On init called ")
+    console.log("heading local reference template div content in ngOnInit ",this.heading.nativeElement.textContent)
+    console.log("Paragraph val in ngOnInit ",this.paragraph.nativeElement.textContent)
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngDoChange called ",changes)
+  }
+  ngDoCheck(): void {
+    console.log("ngDocheck called ")
+  }
+  ngAfterContentInit(): void {
+    console.log("ngAftercontent init called")
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContent checked ")
+    console.log("Paragraph val in ngAfterContent checked",this.paragraph.nativeElement.textContent)
+  }
+  ngAfterViewInit(): void {
+    console.log("ngafter view init called ")
+    console.log("heading local reference template div content in ngViewInit ",this.heading.nativeElement.textContent)
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfter view checked")
+  }
+  ngOnDestroy(): void {
+    console.log('ngOn destroy calleds')
+  }
 }
