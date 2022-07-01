d710d619e990605d58cd1f62a92c0414eb0178e6
chinmay-parkhi <chinmay.parkhi@forcepoint.com>
2022-07-01 19:38:14 +0530

    basic structural directive(ngIf), attribute directive (ngClass,ngStyle), custom attribute directive with property binding,host binding and host listener

---
+++ angular-udemy/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor : string;
+  constructor(private elRef :ElementRef,private renderer : Renderer2) { }
+
+  ngOnInit(): void {
+    //this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue')
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseover(event:Event){
+    //this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue')
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(event:Event){
+   // this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent')
+   this.backgroundColor = this.defaultColor;
+  }
+
+
+}
