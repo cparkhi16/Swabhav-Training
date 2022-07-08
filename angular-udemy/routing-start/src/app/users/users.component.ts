39e878f14fcb58fe99144f2803b923495b77a213
chinmay-parkhi <chinmay.parkhi@forcepoint.com>
2022-07-08 13:51:45 +0530

    Using router-outlet to load component based on current path , using routerlink directive for handling navigation , registering routes in app module

---
+++ angular-udemy/routing-start/src/app/users/users.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+  constructor(){
+    console.log("--=- users components -=-=")
+  }
+  users = [
+    {
+      id: 1,
+      name: 'Max'
+    },
+    {
+      id: 2,
+      name: 'Anna'
+    },
+    {
+      id: 3,
+      name: 'Chris'
+    }
+  ];
+}
