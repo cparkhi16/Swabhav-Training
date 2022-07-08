b91c955ab71a17d01911d4d9160b392f4c96126f
chinmay-parkhi <chinmay.parkhi@forcepoint.com>
2022-07-08 20:58:02 +0530

    Using relative path with navigate method of router

--- angular-udemy/routing-start/src/app/servers/servers.component.ts
+++ angular-udemy/routing-start/src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,13 +10,17 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { 
+  constructor(private serversService: ServersService,private router: Router,private route: ActivatedRoute) { 
     console.log('hi---------')
   }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
     console.log("this servers ",this.servers)
+  }
+
+  onReload(){
+    // this.router.navigate(['servers'],{relativeTo: this.route}) navigate will not know current path so we need to give the extra js object for relative path
   }
 
 }
