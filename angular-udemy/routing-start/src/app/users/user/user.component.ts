6f14c7d1c47aef12d4f56a1a63ab8e961ff4577f
chinmay-parkhi <chinmay.parkhi@forcepoint.com>
2022-07-11 14:25:39 +0530

    unsubscribing observable in component

--- angular-udemy/routing-start/src/app/users/user/user.component.ts
+++ angular-udemy/routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit ,OnDestroy{
   user: {id: number, name: string};
+  parameterSubscription : Subscription;
 
   constructor(private route: ActivatedRoute) {
     console.log("uSER COMPONENT ",this.user)
@@ -18,13 +20,18 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name : this.route.snapshot.params['name']
     }
-    this.route.params.
+    this.parameterSubscription = this.route.params.
     subscribe((params:Params)=>{
       this.user= {
         id: params['id'],
         name : params['name']
       }
     })
+  }
+
+  ngOnDestroy(): void {
+    console.log("Unsubscribing params obs -=-------==")
+    this.parameterSubscription.unsubscribe();
   }
 
 }
