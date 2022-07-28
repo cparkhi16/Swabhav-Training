dcffb3f2a2401e89049cd20864990dbe2415638a
chinmay-parkhi <chinmay.parkhi@forcepoint.com>
2022-07-28 20:20:08 +0530

    Using filter operator to decide whether to send data to next operator/subscription and map operator to do changes to the data to be sent to subscription

--- angular-udemy/obs-01-start/src/app/home/home.component.ts
+++ angular-udemy/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Observable, Subscription } from 'rxjs';
+import {map,filter} from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -26,8 +27,14 @@ export class HomeComponent implements OnInit ,OnDestroy{
         count++;
       },1000)
     })
-
-    this.firstObsSubscription =customIntervalObservable.subscribe(data=>{
+    // customIntervalObservable.pipe(map((data:number) =>{
+    //   return 'Round: '+ (data +1) ;
+    // }))
+    this.firstObsSubscription =customIntervalObservable.pipe(filter(data=>{
+      return data > 0;
+    }),map((data:number) =>{
+      return 'Round: '+ (data +1) ;
+    })).subscribe(data=>{
       console.log(data)
     },error =>{
       console.log(error)
