c9b71bb190f26046eeda9147d431320c435d9824
chinmay-parkhi <chinmay.parkhi@forcepoint.com>
2022-08-01 18:55:02 +0530

    using @ViewChild for getting ngForm access in ts code

--- angular-udemy/forms-td-start/src/app/app.component.ts
+++ angular-udemy/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,10 +7,14 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
-  onSubmit(form: NgForm){
-    console.log("submitted",form)
+  // onSubmit(form: NgForm){
+  //   console.log("submitted",form)
+  // }
+  onSubmit(){
+    console.log("submitted",this.signUpForm)
   }
 }
